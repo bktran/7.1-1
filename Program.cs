067962fb3043ae5d7dd7a502b613a7167f01d5d8
Bao Khanh Tran <ktbv.22@outlook.com>
2026-02-23 12:15:35 -0800

    updated oprogram.cs and added screenshot

--- Program.cs
+++ Program.cs
@@ -4,20 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = { 3, 2, 5, 4, 1 };
+            SelectionSort(array);
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+
         }
 
         public static void SelectionSort(int[] array)
         {
+
             for (int i = 0; i < array.Length; i++)
             {
                 int minIndex = i;
+                //loop to find index less than i
                 for (int j = i + 1; j < array.Length; j++)
                 {
+                    //update min index
                     if (array[j] < array[minIndex])
                     {
                         minIndex = j;
                     }
+                    //swap if differnt from i
                     if (minIndex != i)
                     {
                         int temp = array[i];
